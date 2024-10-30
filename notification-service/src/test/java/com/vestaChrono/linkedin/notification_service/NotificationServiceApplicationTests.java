b362ff1ac222e9a23bd1345731b6ecbf51f974f4
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-10-30 08:42:17 +0530

    5.0 Adding Kafka service to the project

---
+++ notification-service/src/test/java/com/vestaChrono/linkedin/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vestaChrono.linkedin.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
